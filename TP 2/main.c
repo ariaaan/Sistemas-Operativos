ff20fd956430e89e9673b217135393d5df2f4805
ariaaan <arian2822@gmail.com>
2015-08-29 21:30:54 -0300

    Puede encontrar el path de un programa si existe en algun directorio del /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/arian/ciaa-ide/gcc-arm-none-eabi-4_9-2015q1/bin:/home/arian/ciaa-ide/openocd-0.9.0/src

--- TP 2/main.c
+++ TP 2/main.c
@@ -1,60 +1,127 @@
+/*
+* Sistemas Operativos I.
+* ----------------------
+*
+* Trabajo Práctico Nº 2.
+* 
+* Alumno: Giles García, Arian.
+* Matrícula: 36201187.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #define BUFFERSIZE 256
+#define PATHLENGTH 32
 
-void getPath(char *command);
+/*
+* Declaración de funciones
+*/
+
+void get_path(char *command);
+
+/*
+* Declaración de variables globales 
+*/
+
+//Array donde voy a guardar las rutas de la variable PATH
+char path_array[PATHLENGTH][BUFFERSIZE]; 
+
+//Contador de cantidad de rutas en PATH
+int length = 0;
 
 int main(int argc, char **argv) {
 
+
+
+	//Guardo en path el valor de la variable de entorno PATH.
+	char *path;
+	path = getenv("PATH");
+
+	//Divido en ":" y encuentro todos las rutas incluidas en PATH.
+	char *token;
+	char *divider = ":";
+
+	token = strtok (path, divider);
+
+	//Hasta que no haya más rutas, las agrego a "path_array"
+	while (token != NULL) {
+		//Guardo la ruta en "path_array"
+		strcpy(path_array[length], token);
+		
+		//Debug option
+		//printf("path_array[%d] -> %s\n", length, path_array[length]);
+
+		//Aumento en uno el contador
+		length++;
+
+		//Busco el próximo token
+		token = strtok (NULL, divider);
+	}
+
+	//Variable que indica si hay que terminar el programa
 	int exit = 0;
 
+	//Varaible auxiliar que almacena el comando ingresado
 	char command[BUFFERSIZE];
 
+	//Loop principal del 'baash'
 	while(!exit) {
 		printf("prompt: ");
    		scanf("%s", command);
 
-   		getPath(command);
+   		get_path(command);
 	}	
 
 	return 0;
 }
 
-void getPath(char *command) {
+
+/*
+* Definición de funciones
+*/
+
+void get_path(char *command) {
+
+	//Nos indica si se encontró el programa o no
 	int found = 0;
 
-	char *path = getenv("PATH");
+	//Variable auxiliar
+	char path_aux[BUFFERSIZE];
+	char path[BUFFERSIZE];
 
-	char *token;
-	char *divider = ":";
-	
-	token = strtok (path, divider);
+	int i;
+	for(i = 0; i < length; i++) {
+		strcpy(path_aux, "");
 
-	char filePath[BUFFERSIZE];
-
-	while (token != NULL && !found) {
-		filePath[0] = '\0';
-
-		strcat(filePath, token);
-		strcat(filePath, "/");
-		strcat(filePath, command);
-
-		token = strtok (NULL, divider);
-
-		if(access(filePath, F_OK) != -1 ) {
-			printf("File exists in: %s\n", filePath);
+		//Armo el path completo
+		strcat(path_aux, path_array[i]);
+		strcat(path_aux, "/");
+		strcat(path_aux, command);
+		
+		//Veo si el programa existe
+		if(access(path_aux, F_OK) != -1 ) {
+			//Si existe copio el path en 'path'
+			strcpy(path, path_aux);
+			//Pongo la bandera de 'found' en 1
 			found = 1;
+			//Salgo del loop
 			break;
 		} else {
-			//If it doesn't exist, do nothing
+			//Si no existe no hago nada
 		}
 	}
 
-	if(!found) {
+	//Si no lo encontre
+	if(found) {
+		//Imprimo que existe y su ubicación
+		printf("File exists in: %s\n", path_aux);
+	} else {
+		//Imprimo que no existe
 		printf("Couldn't find file\n");
+
 	}
 
 }
