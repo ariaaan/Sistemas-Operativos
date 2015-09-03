29f260217802063d2d15082e3a13223d8cb478e0
ariaaan <arian2822@gmail.com>
2015-09-03 20:59:56 -0300

    Varios fixes

--- TP 2/main.c
+++ TP 2/main.c
@@ -3,7 +3,7 @@
 * ----------------------
 *
 * Trabajo Práctico Nº 2.
-* 
+*
 * Alumno: Giles García, Arian.
 * Matrícula: 36201187.
 */
@@ -23,17 +23,24 @@
 void get_path(char *command);
 
 /*
-* Declaración de variables globales 
+* Declaración de variables globales
 */
 
 //Array donde voy a guardar las rutas de la variable PATH
-char path_array[PATHLENGTH][BUFFERSIZE]; 
+char path_array[PATHLENGTH][BUFFERSIZE];
 
 //Contador de cantidad de rutas en PATH
 int length = 0;
 
 int main(int argc, char **argv) {
 
+	//Busco el current working directory y lo guardo
+	char cwd[1024];
+   	if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    	fprintf(stdout, "\nCurrent Dir: %s\n\n", cwd);
+   	} else {
+       	perror("getcwd() error");
+   	}
 
 
 	//Guardo en path el valor de la variable de entorno PATH.
@@ -50,7 +57,7 @@ int main(int argc, char **argv) {
 	while (token != NULL) {
 		//Guardo la ruta en "path_array"
 		strcpy(path_array[length], token);
-		
+
 		//Debug option
 		//printf("path_array[%d] -> %s\n", length, path_array[length]);
 
@@ -69,11 +76,11 @@ int main(int argc, char **argv) {
 
 	//Loop principal del 'baash'
 	while(!exit) {
-		printf("prompt: ");
+		printf("arian@baash: ");
    		scanf("%s", command);
 
    		get_path(command);
-	}	
+	}
 
 	return 0;
 }
@@ -100,7 +107,7 @@ void get_path(char *command) {
 		strcat(path_aux, path_array[i]);
 		strcat(path_aux, "/");
 		strcat(path_aux, command);
-		
+
 		//Veo si el programa existe
 		if(access(path_aux, F_OK) != -1 ) {
 			//Si existe copio el path en 'path'
