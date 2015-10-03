0344525dbf6c280f226a17b3e1791ea65c67cdd0
ariaaan <arian2822@gmail.com>
2015-10-03 15:03:27 -0300

    Agregue decrypter

--- TP-3/driver_encrypter.c
+++ TP-3/driver_encrypter.c
@@ -4,39 +4,73 @@
 #include <linux/string.h>
 #include <asm/uaccess.h>
 
+/* Module Information */
+/* ------------------ */
+
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Device Driver Encryptor");
 MODULE_AUTHOR("Arian Giles Garcia");
 
+/* Variables */
+/* --------- */
+
+/* Major Number of the Driver */
 static int major_number = 256;
 
+/* Amount that is added to every char of the data */
 static int encryptation_seed = 1;
 
+/* Data to be encripted */
 static char data[100] = {0};
+
+/* Encripted data */
 static char encrypted_data[100] = {0};
 
-void encrypt_data(void) {
-	int k;
-	for(k = 0; k < strlen(data); k++) {
-		encrypted_data[k] = data[k] + encryptation_seed;
-	}
-}
+
+/* Functions Declarations */
+/* ---------------------- */
+int dev_open(struct inode *pinode, struct file *pfile);
+ssize_t dev_read (struct file *pfile, char __user *buffer, size_t length, loff_t *offset);
+ssize_t dev_write (struct file *pfile, const char __user *buffer, size_t length, loff_t *offset);
+int dev_close (struct inode *pinode, struct file *pfile);
+
+void encrypt_data(void);
+
+/* File Operations Structure */
+/* ------------------------- */
+
+struct file_operations fops = {
+	.open = dev_open,
+	.read = dev_read,
+	.write = dev_write,
+	.release = dev_close
+};
 
 /* File Operations Functions */
 /* ------------------------- */
 
 /* Device Open */
 int dev_open(struct inode *pinode, struct file *pfile) {
-	printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+	printk(KERN_ALERT "Device was opened.\n");
 	return 0;
 }
 
 /* Device Read */
 ssize_t dev_read (struct file *pfile, char __user *buffer, size_t length, loff_t *offset) {
-	printk(KERN_ALERT "Inside the %s function.\n", __FUNCTION__);
-	printk(KERN_ALERT "Data: %s\n", data);
+	int k;
+	int bytes_read = 0;
+
+	printk(KERN_ALERT "Read Data: %s\n", data);
 	printk(KERN_ALERT "Encrypted Data: %s\n", encrypted_data);
-	return 0;
+
+	for(k = 0; k < strlen(encrypted_data); k++) {
+		put_user(encrypted_data[k], buffer++);
+		bytes_read++;
+	}
+
+	printk(KERN_ALERT "bytes_read: %d\n", bytes_read);
+
+	return bytes_read;
 }
 
 /* Device Write */
@@ -54,40 +88,36 @@ ssize_t dev_write (struct file *pfile, const char __user *buffer, size_t length,
 
 /* Device Close */
 int dev_close (struct inode *pinode, struct file *pfile) {
-	printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+	printk(KERN_ALERT "Device was closes\n");
 	return 0;
 }
 
-
-
-/* File Operations Structure */
-/* ------------------------- */
-
-struct file_operations fops = {
-	.open = dev_open,
-	.read = dev_read,
-	.write = dev_write,
-	.release = dev_close
-};
-
 /* Driver Init */
 int driver_init(void) {
-	printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+	printk(KERN_ALERT "Device init\n");
 
 	/* Register with the kernel and indicate 
 	that we are registering a char device*/
 
 	/* Parameteres: Major Number, Name of the driver, File operations */ 
-	register_chrdev(major_number, "encryptor", &fops); 
+	register_chrdev(major_number, "encrypter", &fops); 
 	return 0;
 }
 
 /* Driver Exit */
 void driver_exit(void) {
-	printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+	printk(KERN_ALERT "Device exit\n");
 
 	/* Unregister the char device driver */
-	unregister_chrdev(major_number, "encryptor");
+	unregister_chrdev(major_number, "encrypter");
+}
+
+/* Encrypt data */
+void encrypt_data(void) {
+	int k;
+	for(k = 0; k < strlen(data); k++) {
+		encrypted_data[k] = data[k] + encryptation_seed;
+	}
 }
 
 module_init(driver_init);
