b64e0de0ec3661ebceb2f10115b3d27251f2c3ad
Arian Giles Garcia <arian2822@gmail.com>
2015-11-06 19:33:20 -0300

    Agregue lista doblemente enlazada, agregue malloc simple

--- TP-4/main.c
+++ TP-4/main.c
@@ -23,6 +23,8 @@ struct mem_block {
 void *malloc(size_t size);
 void print_memory_list();
 block find_block(size_t size);
+block extend_heap(size_t size);
+void split_block(size_t size);
 
 /* Inicio de la lista */
 void *memory_list_head = NULL;
@@ -31,23 +33,14 @@ void *memory_list_tail = NULL;
 
 int main(int argc, char *argv[]){
 
-	malloc(10);
-	malloc(13);
-	malloc(13);
-	malloc(10);
-	malloc(10);
-	malloc(13);
-	malloc(13);
-	malloc(10);
-	malloc(10);
-	malloc(13);
-	malloc(13);
-	malloc(10);
+	malloc(45);
+	malloc(16);
+	malloc(146);
+	malloc(79);
+	malloc(255);
+	malloc(25);
 
 	print_memory_list();
-
-	// block my_block = memory_list_head;
-	// printf("Size of head block: %zu\n", my_block->size);
 
 	return 0;
 }
@@ -55,63 +48,66 @@ int main(int argc, char *argv[]){
 void *malloc(size_t size){
 	/* Primero encuentro el valor mas cercano multiplo de 4 bytes (32 bits) para alocar */
 	size_t aligned_size = align4(size);
+	block new_block, last_block;
 
 	/* Me fijo si es la primera vez que se ejecuta malloc() */
 	if(memory_list_head == NULL) {
-		/* Creo un nuevo bloque */
-		block new_block;
+		/* Llamo a extend_heap */
+		new_block = extend_heap(aligned_size);
 
-		/* Obtengo la posicion del break y pongo el nuevo bloque ahi */
-		new_block = sbrk(0);
+		/* Si no se pudo extender el heap retorno NULL */
+		if(!new_block) return NULL;
 
-		/* Intento mover el break en el size alineado, si falla retorno NULL */
-		if (sbrk( BLOCK_SIZE + size) == (void*) -1) {
-			printf("Error\n");
-			return (NULL);
-		}
-
-		/* Si pude mover el break, anoto los datos en la estructura */
-		new_block->size = aligned_size;
-		new_block->is_free = 0;
-		new_block->next = NULL;
-		new_block->prev = NULL;
-
-		/* Pongo al nuevo bloque como la cabeza de la lista */
+		/* Si todo salio bien, pongo al nuevo bloque como la cabeza y cola de la lista */
 		memory_list_head = new_block;
 		memory_list_tail = new_block;
 	} else {
 		/* Si ya existe una lista */
 
 		/* Veo si hay un espacio libre en la lista en la que entre */
-		block my_block = find_block(size);
+		block my_block = NULL;
+		my_block = find_block(size);
 
-		/* Si hay, lo ocupo (Todo el tamaño por ahora) */
 		if(my_block != NULL) {
-			my_block->size = aligned_size;
-			my_block->is_free = 0;
+			printf("NO NULL! -> ");
+			printf("Size: %zu -> Free: %s -> Prev: %p -> Next: %p \n", my_block->size,  (my_block->is_free) ? "Yes": "No", my_block->prev, my_block->next);
 		} else {
-			/* Creo un nuevo bloque al final */
-			my_block = sbrk(aligned_size);
-			my_block->size = aligned_size;
-			my_block->is_free = 0;
-			my_block->prev = memory_list_tail;
-			my_block->next = NULL;
-
-			my_block->prev->next = my_block;
+			my_block = extend_heap(aligned_size);
+			if(my_block == NULL) return NULL;
 
 			memory_list_tail = my_block;
 		}
 	}
 }
 
+block extend_heap(size_t size) {
+	block new_block = NULL;
+	new_block = sbrk(0);
+
+	if(sbrk(BLOCK_SIZE + size) == (void*)-1) return NULL;
+
+	new_block->size = size;
+	new_block->next = NULL;
+	new_block->prev = memory_list_tail;
+	new_block->is_free = 0;
+
+	if(memory_list_tail != NULL) {
+		block last = memory_list_tail;
+		last->next = new_block;
+	}
+
+	return new_block;
+}
+
 block find_block(size_t size) {
-	// block b = memory_list_head;
-	// /* Recorro la lista */
-	// while(b && !((b->is_free) && (b->size >= size))) {
-	// 	b = b->next;
-	// }
-	//
-	// return b;
+	block my_block = memory_list_head;
+
+	while(my_block != NULL) {
+		if((my_block->size > size) && (my_block->is_free)) return my_block;
+
+		my_block = my_block->next;
+	}
+
 	return NULL;
 }
 
@@ -124,16 +120,16 @@ void print_memory_list() {
 	/* Si no esta vacia, la recorro e imprimo los datos */
 	int index = 0;
 
-	printf("+----------+----------+----------+\n");
-	printf("| %8s | %8s | %8s |\n", "Block Nº", "Size", "Is Free?");
-	printf("+----------+----------+----------+\n");
+	printf("+----------+----------+----------+------------+------------+\n");
+	printf("| %8s | %8s | %8s | %10s | %10s |\n", "Block Nº", "Size", "Is Free?", "Previous", "Next");
+	printf("+----------+----------+----------+------------+------------+\n");
 
 	do {
-		printf("| %8d | %8zu | %8d |\n", index, b->size,  b->is_free);
+		printf("| %8d | %8zu | %8s | %10p | %10p |\n", index, b->size,  (b->is_free) ? "Yes": "No", b->prev, b->next);
 		index++;
 		b = b->next;
 	} while(b != NULL);
 
-	printf("+----------+----------+----------+\n");
+	printf("+----------+----------+----------+------------+------------+\n");
 
 }
